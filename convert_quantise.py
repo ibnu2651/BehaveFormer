f9b60239b2a23bdb2b7a5ace34ed5b45c03b3517
Ibnu Taimiyyah <ibnu2651@gmail.com>
2025-10-29 02:45:06 +0800

    Add other eval tests for quantised model

--- convert_quantise.py
+++ convert_quantise.py
@@ -10,6 +10,39 @@ from torch.utils.data import DataLoader
 import json
 from evaluation.metrics import Metric
 
+def get_periods(user_id, num_enroll_sess, num_verify_sess=None, user_session_count=None, dataset='humi'):
+    def get_window_time_humi(seqs):
+        seq = seqs[0][0]
+        start = seq[0][0]
+        end = seq[-1][0]
+        
+        i = -1
+        while (end == 0):  # handle zero padding at the end
+            end = seq[i-1][0]
+            i = i - 1
+        return (end - start) / 1000
+
+    def get_window_time_feta(seqs):
+        seq = seqs[0].numpy()
+        start = seq[0][0]
+        end = seq[-1][0]
+        
+        i = -1
+        while (end == 0):  # handle zero padding at the end
+            end = seq[i-1][0]
+            i = i - 1
+        return (end - start) / 1000
+
+    # Get 2 period from the same user and 2 from different user
+    periods = []
+    for j in range(num_verify_sess):
+        periods.append(get_window_time_humi(test_dataset.data[user_id][num_enroll_sess + j]))
+    for i in range(len(test_dataset.data)):
+        if (i != user_id):
+            for j in range(num_verify_sess):
+                periods.append(get_window_time_humi(test_dataset.data[i][num_enroll_sess + j]))
+    return periods
+
 model_fp32_path = "/home/i/ibnu2651/BehaveFormer/work_dirs/humi_scroll50down_imu100all_epoch500_enroll3_b128/20231026_155303/best_models/model_fp32.onnx"
 model_preprocess_path = "/home/i/ibnu2651/BehaveFormer/work_dirs/humi_scroll50down_imu100all_epoch500_enroll3_b128/20231026_155303/best_models/model_preprocess.onnx"
 model_int8_path = "/home/i/ibnu2651/BehaveFormer/work_dirs/humi_scroll50down_imu100all_epoch500_enroll3_b128/20231026_155303/best_models/model_int8.onnx"
@@ -99,28 +132,56 @@ num_enroll_sessions = 3
 num_verify_sessions = 2
 
 pt_acc = []
+pt_usability = []
+pt_tcr = []
+pt_fawi = []
+pt_frwi = []
 onnx_quantised_acc = []
+onnx_quantised_usability = []
+onnx_quantised_tcr = []
+onnx_quantised_fawi = []
+onnx_quantised_frwi = []
 
 for i in range(pt_outputs.shape[0]):
     all_ver_embeddings = torch.cat([pt_outputs[i,num_enroll_sessions:], torch.flatten(pt_outputs[:i,num_enroll_sessions:], start_dim=0, end_dim=1), torch.flatten(pt_outputs[i+1:,num_enroll_sessions:], start_dim=0, end_dim=1)], dim=0)
     scores = Metric.cal_session_distance_fixed_sessions(all_ver_embeddings, pt_outputs[i,:num_enroll_sessions])
 
+    periods = get_periods(i, num_enroll_sessions, num_verify_sessions)   #### use num_verify_sessions for period & also skip num_enroll_sessions
+    labels = torch.tensor([1] * num_verify_sessions + [0] * (pt_outputs.shape[0] - 1) * num_verify_sessions)
+
     acc, threshold = Metric.eer_compute(scores[:num_verify_sessions], scores[num_verify_sessions:])
 
-    pt_acc.append(acc)
+    usability = Metric.calculate_usability(scores, threshold, periods, labels)
+    tcr = Metric.calculate_TCR(scores, threshold, periods, labels)
+    frwi = Metric.calculate_FRWI(scores, threshold, periods, labels)
+    fawi = Metric.calculate_FAWI(scores, threshold, periods, labels)
+    pt_acc.append(acc)    
+    pt_usability.append(usability)
+    pt_tcr.append(tcr)
+    pt_fawi.append(fawi)
+    pt_frwi.append(frwi)
+    
 
     all_ver_embeddings = torch.cat([onnx_quantised_outputs[i,num_enroll_sessions:], torch.flatten(onnx_quantised_outputs[:i,num_enroll_sessions:], start_dim=0, end_dim=1), torch.flatten(onnx_quantised_outputs[i+1:,num_enroll_sessions:], start_dim=0, end_dim=1)], dim=0)
     scores = Metric.cal_session_distance_fixed_sessions(all_ver_embeddings, onnx_quantised_outputs[i,:num_enroll_sessions])
 
     acc, threshold = Metric.eer_compute(scores[:num_verify_sessions], scores[num_verify_sessions:])
 
+    usability = Metric.calculate_usability(scores, threshold, periods, labels)
+    tcr = Metric.calculate_TCR(scores, threshold, periods, labels)
+    frwi = Metric.calculate_FRWI(scores, threshold, periods, labels)
+    fawi = Metric.calculate_FAWI(scores, threshold, periods, labels)
     onnx_quantised_acc.append(acc)
+    onnx_quantised_usability.append(usability)
+    onnx_quantised_tcr.append(tcr)
+    onnx_quantised_fawi.append(fawi)
+    onnx_quantised_frwi.append(frwi)
 
 
-print("Pytorch acc: ", 100 - np.mean(pt_acc, axis=0))
-print(pt_acc)
-print("ONNX quantised acc: ", 100 - np.mean(onnx_quantised_acc, axis=0))
-print(onnx_quantised_acc)
+print("Pytorch\nEER:", 100 - np.mean(pt_acc, axis=0), "Usability:", np.mean(pt_usability, axis=0), "TCR:", np.mean(pt_tcr, axis=0), "FRWI:", np.mean(pt_frwi, axis=0) , "FAWI:", np.mean(pt_fawi, axis=0))
+# print(pt_acc)
+print("ONNX quantised\nEER:", 100 - np.mean(onnx_quantised_acc, axis=0), "Usability:", np.mean(onnx_quantised_usability, axis=0), "TCR:", np.mean(onnx_quantised_tcr, axis=0), "FRWI:", np.mean(onnx_quantised_frwi, axis=0) , "FAWI:", np.mean(onnx_quantised_fawi, axis=0))
+# print(onnx_quantised_acc)
 
 
 # outs = pt_outputs[:5]
