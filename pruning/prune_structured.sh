8738f6e40d9072d1526c09eb7236c51a0f4b228b
Ibnu Taimiyyah <ibnu2651@gmail.com>
2026-04-30 00:18:25 +0800

    Rename test roc file

--- pruning/prune_structured.sh
+++ pruning/prune_structured.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=prune_structured
-#SBATCH --output=prune_structured_200_40_3.slurmlog
-#SBATCH --error=prune_structured_200_40_3.slurmlog
+#SBATCH --output=prune_structured_200_40_3_roc.slurmlog
+#SBATCH --error=prune_structured_200_40_3_roc.slurmlog
 #SBATCH --time=02:00:00
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
@@ -12,5 +12,5 @@ config="200_40_3"
 
 # srun python prune_structured.py "$config"
 # srun -u python finetune_structured.py "$config"
-# srun -u python test_structured_finetune.py "$config"
-srun -u python export_to_onnx.py "$config"
+srun -u python test_structured_finetune_roc.py "$config"
+# srun -u python export_to_onnx.py "$config"
